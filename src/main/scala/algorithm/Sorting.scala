aa3d5939a321b093c22e8955d5d1f7929f12fbc3
Hedley Luna <hedleygois@gmail.com>
2016-05-30 00:18:09 -0300

    Just renaming the parameters of the functions to look better

--- src/main/scala/algorithm/Sorting.scala
+++ src/main/scala/algorithm/Sorting.scala
@@ -6,5 +6,5 @@ trait Sorting {
   /**
     * Time Complexity is O(n log(n)) - http://stackoverflow.com/questions/14146990/what-algorithm-is-used-by-the-scala-library-method-vector-sorted
     */
-  def sort(graph: Graph) = graph.structure.map(_._1).toList.sortBy(node => node.failureRate)
+  def sort(graph: Graph) = graph.structure.map(_._1).toList.sortBy(_.failureRate)
 }
