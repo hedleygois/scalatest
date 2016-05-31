1d920088f59bdf07b42caaf4b569965370af49c2
Hedley Luna <hedleygois@gmail.com>
2016-05-30 21:02:37 -0300

    Removing the graph creating from the test

--- src/test/scala/ConnectednessTest.scala
+++ src/test/scala/ConnectednessTest.scala
@@ -8,34 +8,10 @@ class ConnectednessTest extends WordSpec with Matchers {
 
   "A Connectedness algorithm " should {
     "check a not connected graph " in {
-      val first = Node("First", 1.0)
-      val second = Node("Second", 2.0)
-      val third = Node("Third", 1.0)
-      val forth = Node("Forth", 0.5)
-      val fifth = Node("Fifth", 0.3)
-      val graph = Graph(Map(
-        first -> (second :: Nil),
-        second -> (third :: Nil),
-        third -> (forth :: fifth :: Nil),
-        forth -> (Nil),
-        fifth -> (second :: Nil)
-      ))
-      abstractAlgorithm connected(graph) should be (false)
+      abstractAlgorithm connected(DataProvider.notConnectedSmallGraph) should be (false)
     }
     "check a connected graph " in {
-      val first = Node("First", 1.0)
-      val second = Node("Second", 2.0)
-      val third = Node("Third", 1.0)
-      val forth = Node("Forth", 0.5)
-      val fifth = Node("Fifth", 0.3)
-      val graph = Graph(Map(
-        first -> (second :: Nil),
-        second -> (third :: Nil),
-        third -> (forth :: fifth :: Nil),
-        forth -> (first :: Nil),
-        fifth -> (second :: Nil)
-      ))
-      abstractAlgorithm connected(graph) should be (true)
+      abstractAlgorithm connected(DataProvider.connectedSmallGraph) should be (true)
     }
   }
 
