3f54895bfdf973f6cb2f50008a93fcb862e0e9cc
Hedley Luna <hedleygois@gmail.com>
2016-05-30 21:02:16 -0300

    Adding the graph to the parameters list

--- src/test/scala/DFSTest.scala
+++ src/test/scala/DFSTest.scala
@@ -12,7 +12,7 @@ class DFSTest extends WordSpec with Matchers {
     val third = Node("Third", 1.0)
     val forth = Node("Forth", 0.5)
     val fifth = Node("Fifth", 0.3)
-    implicit val graph = Graph(Map(
+    val graph = Graph(Map(
       first -> (second :: forth :: Nil),
       second -> (third :: first :: Nil),
       third -> (forth :: fifth :: Nil),
@@ -21,12 +21,12 @@ class DFSTest extends WordSpec with Matchers {
     ))
 
     "traverse a single graph starting by the first elenent " in {
-      val traversed = abstractAlgorithm traverse (first)
+      val traversed = abstractAlgorithm traverse (first, graph)
       traversed should have size 5
       traversed should equal (first :: second :: third :: forth :: fifth :: Nil)
     }
     "traverse a graph starting on any element " in {
-      val traversed = abstractAlgorithm traverse (third)
+      val traversed = abstractAlgorithm traverse (third, graph)
       traversed should have size 5
       traversed should equal (third :: forth :: fifth :: second :: first :: Nil)
     }
