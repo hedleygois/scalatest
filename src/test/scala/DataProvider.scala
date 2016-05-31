99af0622b3372db8686fe5afdd3d90fdc1781188
Hedley Luna <hedleygois@gmail.com>
2016-05-30 21:01:50 -0300

    Correcting the small graph without bridges because it actually had 2

--- src/test/scala/DataProvider.scala
+++ src/test/scala/DataProvider.scala
@@ -32,10 +32,42 @@ object DataProvider {
     val third = Node("Third", 1.0)
     val forth = Node("Forth", 0.5)
     val graph = Graph(Map(
-      first ->  Nil,
+      first ->  (second :: third :: forth :: Nil),
       second -> (first :: third :: Nil),
-      third -> (first :: Nil),
+      third -> (first :: forth :: Nil),
       forth -> (first :: second :: third :: Nil)
+    ))
+    graph
+  }
+
+  def notConnectedSmallGraph = {
+    val first = Node("First", 1.0)
+    val second = Node("Second", 2.0)
+    val third = Node("Third", 1.0)
+    val forth = Node("Forth", 0.5)
+    val fifth = Node("Fifth", 0.3)
+    val graph = Graph(Map(
+      first -> (second :: Nil),
+      second -> (third :: Nil),
+      third -> (forth :: fifth :: Nil),
+      forth -> (Nil),
+      fifth -> (second :: Nil)
+    ))
+    graph
+  }
+
+  def connectedSmallGraph = {
+    val first = Node("First", 1.0)
+    val second = Node("Second", 2.0)
+    val third = Node("Third", 1.0)
+    val forth = Node("Forth", 0.5)
+    val fifth = Node("Fifth", 0.3)
+    val graph = Graph(Map(
+      first -> (second :: Nil),
+      second -> (third :: Nil),
+      third -> (forth :: fifth :: Nil),
+      forth -> (first :: Nil),
+      fifth -> (second :: Nil)
     ))
     graph
   }
