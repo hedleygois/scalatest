402ac7568197d837fe6670b95818c2bd8c2ebc2d
Hedley Luna <hedleygois@gmail.com>
2016-05-30 21:03:14 -0300

    Little refactoring on the DFS

--- src/main/scala/algorithm/DFS.scala
+++ src/main/scala/algorithm/DFS.scala
@@ -11,22 +11,22 @@ trait DFS {
     * Time complexity O(N)
     *
     */
-  def traverse(initial: Node)(implicit graph: Graph): List[Node] = {
+  def traverse(initial: Node, graph: Graph): List[Node] = {
     def notVisited(initial: Node, visited: List[Node]): List[Node] = {
-      graph structure(initial) filter (node => !visited.contains(node))
+      graph structure(initial) filter (!visited.contains(_))
     }
 
     @tailrec
     def loop(initial: Node, stack: List[Node], visited: List[Node]): List[Node] = {
       val children = notVisited(initial, visited)
-      val childrenWithTail = children ++ stack.tail
+      val childrenWithTail = (children ++ stack.tail).distinct
       val updatedVisited = stack.head :: visited
       if(childrenWithTail isEmpty) updatedVisited
       else loop(childrenWithTail.head, childrenWithTail, updatedVisited)
     }
 
     //TODO Reversing is O(n) and appending on the tail of an immutable list is also O(n) try to change the structure
-    loop(initial, (initial :: Nil),Nil) reverse
+    loop(initial, (initial :: Nil), Nil) reverse
   }
 
 
