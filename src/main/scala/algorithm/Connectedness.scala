83b1b28ef95f5a68661800ab27026999425ba463
Hedley Luna <hedleygois@gmail.com>
2016-06-02 21:36:50 -0300

    Corrected not curried function

--- src/main/scala/algorithm/Connectedness.scala
+++ src/main/scala/algorithm/Connectedness.scala
@@ -3,5 +3,5 @@ package algorithm
 import structure.Graph
 
 trait Connectedness extends DFS {
-  def connected(graph: Graph) = graph.structure.keySet.forall(node => traverse(node)(graph).distinct.size == graph.structure.keySet.size)
+  def connected(graph: Graph) = graph.structure.keySet.forall(node => traverse(node, graph).distinct.size == graph.structure.keySet.size)
 }
